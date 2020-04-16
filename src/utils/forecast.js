d9d26b9d65a4072913154574982a9f786fcab6e9
Giorgio <Giorgio@Clementines-MacBook-Pro.local>
2020-04-16 10:24:21 +0200

    Forecast info added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -24,7 +24,10 @@ const forecast = (latitude, longitude, callback) => {
           body.current.temperature +
           " degrees out. It feels like " +
           body.current.feelslike +
-          " degrees out."
+          " degrees out." +
+          "The humidity is " +
+          body.current.humidity +
+          "%"
       );
     }
   });
